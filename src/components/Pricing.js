bd75490783de99c5b79c4f2bb32b438d0c786b63
Kush Munot <munot.kgm@gmail.com>
2024-08-25 22:59:33 +0530

    Bulllets added

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,5 +1,7 @@
-import { Button, Grid, Link, Typography } from '@mui/material'
+import { Button, Grid, Link, List, ListItem, ListItemIcon, ListItemText, Typography } from '@mui/material'
 import React from 'react'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+
 
 const title = {
     fontFamily: 'Rubik',
@@ -20,13 +22,21 @@ const text = {
     paddingBottom: '1rem'
 }
 
+const pro_plan = [
+    "Track unlimited birthdays",
+    "Additional reminders 3 days away",
+    "Receive SMS birthday reminder texts",
+    "Visualize upcoming birthdays",
+    "Never check Facebook Birthdays ever again",
+];
+
 const Pricing = () => {
     return (
         <div style={{ padding: '10% 2%' }}>
             <Typography sx={title}>Pricing</Typography>
             <Typography sx={text}>Get complimentary Free 7-days trial 🥳</Typography>
             <Grid container >
-                <Grid item lg={5.5} md={5.5} xs={11} sx={{ height: '240px', border: '2px solid #1976d2', borderRadius: '20px', marginLeft: '3%', backgroundColor: '#ffff', mt: '5px' }}>
+                <Grid item lg={5.5} md={5.5} xs={11} sx={{ height: 'auto', border: '2px solid #1976d2', borderRadius: '20px', marginLeft: '3%', backgroundColor: '#ffff', mt: '5px' }}>
                     <Button sx={{
                         backgroundColor: '#915831',
                         color: 'white',
@@ -41,6 +51,18 @@ const Pricing = () => {
                         <Typography sx={{ textTransform: 'none', fontWeight: '500' }}>Monthly</Typography>
                     </Button>
                     <Typography sx={{ fontSize: '62px', fontFamily: 'Nunito Sans', fontWeight: '700', ml: '5%' }}>$ 1.99 <span style={{ fontSize: '22px', fontFamily: 'Nunito Sans', fontWeight: '700' }}>/per month</span></Typography>
+                    <List sx={{ mt: '5%' }}>
+                        {pro_plan.map((feature, index) => (
+                            <ListItem key={index}>
+                                <ListItemIcon>
+                                    <CheckCircleOutlineIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={feature}
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
                     <Link href='/contact-us'>
 
                         <Button sx={{
@@ -62,7 +84,7 @@ const Pricing = () => {
                         }} > Start for Free </Button>
                     </Link>
                 </Grid>
-                <Grid item lg={5.5} md={5.5} xs={11} sx={{ height: '240px', border: '2px solid #1976d2', borderRadius: '20px', marginLeft: '3%', backgroundColor: '#ffff', mt: '5px' }}>
+                <Grid item lg={5.5} md={5.5} xs={11} sx={{ height: 'auto', border: '2px solid #1976d2', borderRadius: '20px', marginLeft: '3%', backgroundColor: '#ffff', mt: '5px' }}>
                     <Button sx={{
                         backgroundColor: '#915831',
                         color: 'white',
@@ -77,8 +99,19 @@ const Pricing = () => {
                         <Typography sx={{ textTransform: 'none', fontWeight: '500' }}>Annually</Typography>
                     </Button>
                     <Typography sx={{ fontSize: '62px', fontFamily: 'Nunito Sans', fontWeight: '700', ml: '5%' }}>$ 19.99 <span style={{ fontSize: '22px', fontFamily: 'Nunito Sans', fontWeight: '700' }}>/per year</span></Typography>
+                    <List sx={{ mt: '5%' }}>
+                        {pro_plan.map((feature, index) => (
+                            <ListItem key={index}>
+                                <ListItemIcon>
+                                    <CheckCircleOutlineIcon />
+                                </ListItemIcon>
+                                <ListItemText
+                                    primary={feature}
+                                />
+                            </ListItem>
+                        ))}
+                    </List>
                     <Link href='/contact-us'>
-
                         <Button sx={{
                             borderRadius: "5px",
                             border: 0,
